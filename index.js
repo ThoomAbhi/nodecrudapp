09ea3210eebf9d6d9c333bb0d70465f9ebca8e5f
ThoomAbhi <thoomabhisathwika@gmail.com>
2025-06-06 12:09:31 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const productRoute=require('./routes/product.route.js');
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended:fasle}));
+
+
+app.use("/api/products",productRoute);
+
+
+
+
+
+
+
+
+mongoose.connect("mongodb+srv://thoomabhisathwika:LRdicI73gwSBVvpu@backend.ihhfzn7.mongodb.net/?retryWrites=true&w=majority&appName=backend")
+.then(() => {
+    console.log("connected");
+})
+.catch(() =>{
+    console.log("failed");
+});
 
 app.get('/',(req,res) => {
     res.send("Hello from nodejs");
